7f86eb4e689e66cecb86f4c44e646139936975a5
Vini Maheshwari <vini.maheshwari@getfareye.com>
2020-06-08 05:59:04 +0000

    first commit

---
+++ src/main/java/com/example/superbiz/SuperbizApplication.java
@@ -0,0 +1,13 @@
+package com.example.superbiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperbizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperbizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/superbiz/SuperbizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.superbiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperbizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
